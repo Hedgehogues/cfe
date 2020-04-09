8e53123f382eb97199d6434d2ad703b54ed17910
hedgehogues <hedgehogues@bk.ru>
2020-04-09 13:55:27 +0300

    [feature]. add function of transforming []Extract to []string

--- core/utils.go
+++ core/utils.go
@@ -1,9 +1,18 @@
 package core
 
-func toStrings(e []*Extract) []string {
+func pToStrings(e []*Extract) []string {
 	var objs []string
 	for _, el := range e {
 		objs = append(objs, el.Object)
 	}
 	return objs
 }
+
+func toStrings(e []Extract) []string {
+	var objs []string
+	for _, el := range e {
+		objs = append(objs, el.Object)
+	}
+	return objs
+}
+
--- core/utils_test.go
+++ core/utils_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_toStrings(t *testing.T) {
+func Test_pToStrings(t *testing.T) {
 	type args struct {
 		e []*Extract
 	}
@@ -31,8 +31,41 @@ func Test_toStrings(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if got := pToStrings(tt.args.e); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pToStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_toStrings(t *testing.T) {
+	type args struct {
+		e []Extract
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "",
+			args: args{
+				[]Extract{
+					{
+						Object:"123",
+					},
+					{
+						Object:"456",
+					},
+				},
+			},
+			want: []string{"123", "456"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			if got := toStrings(tt.args.e); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("toStrings() = %v, want %v", got, tt.want)
+				t.Errorf("pToStrings() = %v, want %v", got, tt.want)
 			}
 		})
 	}
